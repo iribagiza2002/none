b1805602da08ab3eb246285761c20201ab098a1b
murepictures <murepictures@gmail.com>
2024-11-28 15:01:07 +0200

    hfhfh

---
+++ amazing.bat
@@ -0,0 +1,7 @@
+color 2
+@echo off
+:loop
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+%random%%random%%random%%random%%random%%random%%random%%random%
+%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto loop
